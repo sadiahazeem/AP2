472e32b88a30677b24689c15851ef1763d53e414
sadiahazeem <sadiah.azeem@tu-dortmund.de>
2022-06-08 10:07:16 +0200

    vxxx ersetzen

---
+++ YoungAlphaDerEchteDuWeisst/latex-template/v701/berechnungen.py
@@ -0,0 +1,8 @@
+from struct import unpack
+import matplotlib
+matplotlib.use('MacOSX', force=True)
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import uncertainties as unp
+
